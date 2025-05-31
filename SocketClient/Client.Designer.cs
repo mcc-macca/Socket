e4f8f5bc8177ae460607ccfb519279bbfd1779b5
Erick Daniel Parra Delgado (macca) <macca@maccacomputer.com>
2025-05-31 17:32:22 +0200

    Add project files.

---
+++ SocketClient/Client.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace SocketClient {
+    partial class Client {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent() {
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboIpClient = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textIpServer = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.numPort = new System.Windows.Forms.NumericUpDown();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textInvio = new System.Windows.Forms.TextBox();
+            this.btnInvia = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textRx = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.numPort)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "IP Client:";
+            // 
+            // comboIpClient
+            // 
+            this.comboIpClient.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboIpClient.FormattingEnabled = true;
+            this.comboIpClient.Location = new System.Drawing.Point(96, 10);
+            this.comboIpClient.Name = "comboIpClient";
+            this.comboIpClient.Size = new System.Drawing.Size(162, 24);
+            this.comboIpClient.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "IP Server:";
+            // 
+            // textIpServer
+            // 
+            this.textIpServer.Location = new System.Drawing.Point(96, 40);
+            this.textIpServer.Name = "textIpServer";
+            this.textIpServer.Size = new System.Drawing.Size(162, 22);
+            this.textIpServer.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(16, 73);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(42, 16);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Porta:";
+            // 
+            // numPort
+            // 
+            this.numPort.Location = new System.Drawing.Point(96, 73);
+            this.numPort.Name = "numPort";
+            this.numPort.Size = new System.Drawing.Size(162, 22);
+            this.numPort.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(265, 13);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(78, 16);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Messaggio:";
+            // 
+            // textInvio
+            // 
+            this.textInvio.Location = new System.Drawing.Point(268, 40);
+            this.textInvio.Name = "textInvio";
+            this.textInvio.Size = new System.Drawing.Size(520, 22);
+            this.textInvio.TabIndex = 7;
+            // 
+            // btnInvia
+            // 
+            this.btnInvia.Location = new System.Drawing.Point(712, 69);
+            this.btnInvia.Name = "btnInvia";
+            this.btnInvia.Size = new System.Drawing.Size(75, 23);
+            this.btnInvia.TabIndex = 8;
+            this.btnInvia.Text = "Invia";
+            this.btnInvia.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(16, 118);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(127, 16);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Risposta del server:";
+            // 
+            // textRx
+            // 
+            this.textRx.Location = new System.Drawing.Point(19, 138);
+            this.textRx.Multiline = true;
+            this.textRx.Name = "textRx";
+            this.textRx.ReadOnly = true;
+            this.textRx.Size = new System.Drawing.Size(768, 300);
+            this.textRx.TabIndex = 10;
+            // 
+            // Client
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textRx);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btnInvia);
+            this.Controls.Add(this.textInvio);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.numPort);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textIpServer);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboIpClient);
+            this.Controls.Add(this.label1);
+            this.Name = "Client";
+            this.Text = "Socket Client";
+            ((System.ComponentModel.ISupportInitialize)(this.numPort)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboIpClient;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textIpServer;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown numPort;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textInvio;
+        private System.Windows.Forms.Button btnInvia;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textRx;
+    }
+}
+
---
+++ SocketClient/Client.cs
@@ -0,0 +1,133 @@
+﻿using SocketClient;
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SocketClient {
+    public partial class Client : Form {
+        public Client() {
+            InitializeComponent();
+
+            #region Inizializzo i componenti
+
+            // Ottengo il nome dell'host locale
+            string hostname = Dns.GetHostName();
+
+            // Imposto il range e il valore di default della porta
+            numPort.Maximum = 36000;
+            numPort.Value = 11000;
+
+            // Recupero tutti gli indirizzi IP dell'host e li inserisco nella combo
+            IPAddress[] ipAddresses = Dns.GetHostAddresses(hostname);
+            comboIpClient.Items.Clear();
+            foreach (IPAddress ip in ipAddresses) {
+                if (ip.ToString().StartsWith("169.254.")) continue; // Escludo IP automatici
+                if (ip.AddressFamily == AddressFamily.InterNetwork) {
+                    comboIpClient.Items.Add(ip.ToString()); // Solo IPv4
+                }
+            }
+
+            // Seleziono il primo IP disponibile
+            if (comboIpClient.Items.Count > 0) {
+                comboIpClient.SelectedIndex = 0;
+            }
+
+            // Messaggio di avvio client
+            textRx.AppendLine("[SOCKET] > CLIENT AVVIATO");
+
+            #endregion
+        }
+
+        private void btnInvia_Click(object sender, EventArgs e) {
+            #region Verifico i dati ricevuti e li mando o come testo o come JSON
+            // Rimuovo spazi e controllo che il messaggio non sia vuoto
+            textInvio.Text = textInvio.Text.Trim();
+            if (string.IsNullOrEmpty(textInvio.Text)) {
+                MessageBox.Show("Inserire un messaggio da inviare", "Errore", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            IPAddress ipAddress;
+            try {
+                // Controllo e parso l'indirizzo IP del server
+                textIpServer.Text = textIpServer.Text.Trim();
+                ipAddress = IPAddress.Parse(textIpServer.Text);
+            }
+            catch (Exception ex) {
+                MessageBox.Show(ex.Message, "Errore", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            // Ottengo la data e ora attuale
+            string date = DateTime.Now.ToString("dd-MM-yyyy HH:mm:ss");
+
+            string messageToSend;
+
+            // Costruisco il messaggio da inviare
+            messageToSend = $"{Dns.GetHostName()} | {comboIpClient.Text} | {date} | {textInvio.Text}";
+
+            // Loggo il messaggio in uscita
+            textRx.AppendLine($"[CLIENT] {date} > Inviando messaggio...");
+            textRx.AppendLine($"[CLIENT] Messaggio: {messageToSend}");
+            #endregion
+
+            #region Invio del messaggio al server
+            Socket clientSocket = null;
+            try {
+                // Creo un endpoint remoto e socket TCP
+                IPEndPoint remoteEP = new IPEndPoint(ipAddress, (int)numPort.Value);
+                clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                clientSocket.Connect(remoteEP);
+
+                // Verifico la connessione
+                if (!clientSocket.Connected) {
+                    MessageBox.Show("Connessione al server non riuscita.", "Errore", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    textRx.AppendLine("[CLIENT] " + date + " > CONNESSIONE AL SERVER NON RIUSCITA.");
+                    return;
+                }
+
+                // Invio il messaggio al server
+                byte[] msg = Encoding.UTF8.GetBytes(messageToSend + Environment.NewLine);
+                clientSocket.Send(msg);
+
+                // Ricevo la risposta dal server
+                byte[] buffer = new byte[1024];
+                int bytesRec = clientSocket.Receive(buffer);
+
+                if (bytesRec > 0) {
+                    string response = Encoding.UTF8.GetString(buffer, 0, bytesRec);
+                    textRx.AppendLine("[SERVER] " + date + " > " + response);
+                }
+            }
+            catch (Exception ex) {
+                // Gestione errori di connessione o invio/ricezione
+                MessageBox.Show("Errore:\n" + ex, "Errore", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally {
+                // Chiudo la connessione in modo sicuro
+                if (clientSocket != null) {
+                    clientSocket.Shutdown(SocketShutdown.Both);
+                    clientSocket.Close();
+                }
+                textRx.AppendLine("[CLIENT] " + date + " > CONNESSIONE CHIUSA CON SUCCESSO.");
+            }
+            #endregion
+        }
+    }
+
+    public static class WinFormsExtension {
+        #region Funzione AppendLine per TextBox
+        // Estensione per aggiungere una riga a una TextBox
+        public static void AppendLine(this TextBox source, string value) {
+            if (source.Text.Length == 0) {
+                source.Text = value;
+            }
+            else {
+                source.AppendText("\r\n" + value);
+            }
+        }
+        #endregion
+    }
+}
